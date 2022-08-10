bc70b2a7b30b7a64f257eb26ca1b8a645ad9d858
Dennis Akagha <d.akagha20@gmail.com>
2022-08-10 23:08:37 +0100

    Fix linter errors

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,15 +8,15 @@ class GroupsController < ApplicationController
   end
 
   def create
-    render plain: params[:group].inspect
-    # @group = Group.new(group_params)
-    # @group.user_id = current_user.id
-    # if @group.save
-    #   flash[:success] = 'Group created succesfully'
-    #   redirect_to user_groups_path(current_user)
-    # else
-    #   render 'new'
-    # end
+    # render plain: params[:group].inspect
+    @group = Group.new(group_params)
+    @group.user_id = current_user.id
+    if @group.save
+      flash[:success] = 'Group created succesfully'
+      redirect_to user_groups_path(current_user)
+    else
+      render 'new'
+    end
   end
 
   def show; end
@@ -26,5 +26,4 @@ class GroupsController < ApplicationController
   def group_params
     params.require(:group).permit(:name, :icon)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users 
-  devise_scope  :user do
+  devise_for :users
+  devise_scope :user do
     get 'users/sign_out' => 'devise/sessions#destroy'
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users do 
+  resources :users do
     resources :groups do
       resources :expenses
     end
   end
-  root "pages#index"
+  root 'pages#index'
 end
