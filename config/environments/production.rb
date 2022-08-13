2db83670b587a7abca427d64628ab5bc8566a44a
Dennis Akagha <d.akagha20@gmail.com>
2022-08-13 12:16:34 +0100

    Fix bug in database

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => "budget-app-rorails.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "my-budget-app-ror.herokuapp.com" }
 end
