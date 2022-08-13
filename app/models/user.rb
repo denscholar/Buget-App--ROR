0be745618855dffbc802bf828aab963e05e0c20f
Dennis Akagha <d.akagha20@gmail.com>
2022-08-13 12:14:52 +0100

    Fix bug in database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :validatable, :trackable
+  devise :database_authenticatable, :registerable, 
+         :recoverable, :rememberable, :validatable
 
   validates :email, presence: true
   validates :name, presence: true, length: { minimum: 3, maximum: 100 }
