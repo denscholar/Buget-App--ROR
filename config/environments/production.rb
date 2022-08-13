1a7e3c5dd1b52246c2ca1d3ffd821426d65b9590
Dennis Akagha <d.akagha20@gmail.com>
2022-08-13 06:04:51 +0100

    Add config action mail to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => "https://budget-application-ror.herokuapp.com/" }
+  config.action_mailer.default_url_options = { :host => "https://budget-app--ror.herokuapp.com/" }
 end
